687358e3d6fac812fafddf7386df2c9f87023d9c
Aasthaj16 <71823554+Aasthaj16@users.noreply.github.com>
2022-07-15 14:40:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    var flag = false;
+
+
+    $('.clicks').on('click', function () {
+        if (!flag) {
+            flag = true;
+            $('#box').addClass('show');
+        } else {
+            $('#box').removeClass('show');
+            flag = false
+        }
+    })
+
+
+    $('.submission').on('click', function () {
+        flag = false
+        $('#box').removeClass('show');
+    })
+
+    $('.crossButton').on('click', function () {
+        flag = false
+        $('#box').removeClass('show');
+    })
+});
+
